07d7e9761b95a1e11d88614e9ad8c28e02a78f40
BigWave8 <rostykchess@gmail.com>
2020-11-24 12:44:54 +0200

    added 4 lab wchain

---
+++ Wchain/Launcher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Wchain
+{
+    class Launcher
+    {
+        public static Dictionary<string, int> WordsChain { get; set; }
+        public static string[] Words { get; set; }
+        public static int Answer { get; set; }
+        static void Main(string[] args)
+        {
+            const string pathIn = @"D:\projects\Algo\Wchain\wchain_in.txt";
+            const string pathOut = @"D:\projects\Algo\Wchain\wchain_out.txt";
+            WordsChain = new Dictionary<string, int>();
+            Words = ReadWithFile(pathIn);
+            Answer = Algorithm();
+            WriteInFile(pathOut);
+        }
+        public static int Algorithm()
+        {
+            int maxLength = 0;
+            Array.Sort(Words, (a, b) => a.Length - b.Length);
+            foreach (string word in Words)
+            {
+                WordsChain[word] = 1;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    StringBuilder sb = new StringBuilder(word);
+                    sb.Remove(i, 1);
+                    string nextWord = sb.ToString();
+                    if (WordsChain.ContainsKey(nextWord) && WordsChain[word] < WordsChain[nextWord] + 1)
+                    {
+                        WordsChain[word] = WordsChain[nextWord] + 1;
+                    }
+                }
+                maxLength = maxLength > WordsChain[word] ? maxLength : WordsChain[word];
+            }
+            return maxLength;
+        }
+        public static string[] ReadWithFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int wordNumbers = Convert.ToInt32(sr.ReadLine());
+                string[] words = new string[wordNumbers];
+                for (int i = 0; i < wordNumbers; i++)
+                {
+                    words[i] = sr.ReadLine();
+                }
+                sr.Close();
+                return words;
+            }
+        }
+        public static void WriteInFile(string path)
+        {
+            using (StreamWriter sr = new StreamWriter(path, false))
+            {
+                sr.WriteLine(Answer);
+                sr.Close();
+            }
+            Console.WriteLine("Answer in wchain out file");
+        }
+    }
+}
---
+++ Wchain/WchainTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Wchain
+{
+    [TestFixture]
+    class WchainTest
+    {
+        [Test]
+        public void Algorithm_TenWords_Expected6()
+        {
+            string pathTest = @"D:\projects\Algo\Wchain\wchain_test1.txt";
+            Launcher.WordsChain = new Dictionary<string, int>();
+            Launcher.Words = Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answer = Launcher.Algorithm();
+
+            Assert.AreEqual(6, Launcher.Answer);
+        }
+        [Test]
+        public void Algorithm_FiveWords_Expected5()
+        {
+            string pathTest = @"D:\projects\Algo\Wchain\wchain_test2.txt";
+            Launcher.WordsChain = new Dictionary<string, int>();
+            Launcher.Words = Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answer = Launcher.Algorithm();
+
+            Assert.AreEqual(4, Launcher.Answer);
+        }
+        [Test]
+        public void Algorithm_ThreeWords_Expected1()
+        {
+            string pathTest = @"D:\projects\Algo\Wchain\wchain_test3.txt";
+            Launcher.WordsChain = new Dictionary<string, int>();
+            Launcher.Words = Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answer = Launcher.Algorithm();
+
+            Assert.AreEqual(1, Launcher.Answer);
+        }
+        [Test]
+        public void Algorithm_TenWords_Expected10()
+        {
+            string pathTest = @"D:\projects\Algo\Wchain\wchain_test4.txt";
+            Launcher.WordsChain = new Dictionary<string, int>();
+            Launcher.Words = Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answer = Launcher.Algorithm();
+
+            Assert.AreEqual(10, Launcher.Answer);
+        }
+        [Test]
+        public void Algorithm_EightWords_Expected2()
+        {
+            string pathTest = @"D:\projects\Algo\Wchain\wchain_test5.txt";
+            Launcher.WordsChain = new Dictionary<string, int>();
+            Launcher.Words = Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answer = Launcher.Algorithm();
+
+            Assert.AreEqual(2, Launcher.Answer);
+        }
+    }
+}
