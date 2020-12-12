f0429fa7790e13ed9b92d247d795fda08683f520
BigWave8 <rostykchess@gmail.com>
2020-12-12 22:21:44 +0200

    added 5 lab - search substring in string

---
+++ SearchAlgorithm/Launcher.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Launcher
+    {
+        public static string InputString { get; set; }
+        public static string Pattern { get; set; }
+        public static int[] Answers { get; set; }
+        static void Main(string[] args)
+        {
+            const string pathIn = @"D:\projects\Algo\SearchAlgorithm\string_test5.txt";
+            const string pathOut = @"D:\projects\Algo\SearchAlgorithm\string_out.txt";
+            ReadWithFile(pathIn);
+            Answers = (int[])BruteForceAlgorithm().ToArray().Clone();
+            WriteInFile(pathOut);
+        }
+
+        public static void LaunchAlgorithms()
+        {
+            
+        }
+        public static List<int> BruteForceAlgorithm()
+        {
+            List<int> answers = new List<int>();
+            if (Pattern.Length > InputString.Length)
+            {
+                answers.Add(-1);
+                return answers;
+            }
+            for (int i = 0; i <= InputString.Length - Pattern.Length; i++)
+            {
+                for (int j = 0; j < Pattern.Length; j++)
+                {
+                    if (InputString[i+j] != Pattern[j])
+                        break;
+                    if (j == Pattern.Length - 1)
+                    {
+                        answers.Add(i);
+                    }
+                }
+            }
+            if (answers.Count == 0)
+                answers.Add(-1);
+            return answers;
+        }
+        public static List<int> BoyerMooreAlgorithm()
+        {
+            List<int> answers = new List<int>();
+            if (Pattern.Length > InputString.Length)
+            {
+                answers.Add(-1);
+                return answers;
+            }
+            int skip;
+            for (int i = 0; i <= InputString.Length - Pattern.Length; i+=skip)
+            {
+                skip = 0;
+                for (int j = Pattern.Length - 1; j >= 0; j--)
+                {
+                    if (InputString[i + j] != Pattern[j])
+                    {
+                        skip = Math.Max(1, j);
+                        break;
+                    }
+                }
+                if (skip == 0)
+                {
+                    answers.Add(i);
+                    skip = Pattern.Length;
+                }
+            }
+            if (answers.Count == 0)
+                answers.Add(-1);
+            return answers;
+        }
+        public static void ReadWithFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var array = sr.ReadLine().Split(' ').ToArray();
+                InputString = array[0];
+                Pattern = array[1];
+                sr.Close();
+            }
+        }
+        public static void WriteInFile(string path)
+        {
+            using (StreamWriter sr = new StreamWriter(path, false))
+            {
+                foreach (var answer in Answers)
+                {
+                    sr.WriteLine($"{answer} --> {answer + Pattern.Length}" );
+                }
+                sr.Close();
+            }
+            Console.WriteLine("Answer in string out file");
+        }
+    }
+}
---
+++ SearchAlgorithm/TestSearch.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SearchAlgorithm
+{
+    [TestFixture]
+    class TestSearch
+    {
+        [Test]
+        public void BruteForceAlgorithm_DDABA_Expected14()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test1.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BruteForceAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(14, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BruteForceAlgorithm_5_Expected5()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test2.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BruteForceAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(4, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BruteForceAlgorithm_CAB_Expected2_5_8_11()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test3.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BruteForceAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(2, Launcher.Answers[0]);
+            Assert.AreEqual(5, Launcher.Answers[1]);
+            Assert.AreEqual(8, Launcher.Answers[2]);
+            Assert.AreEqual(11, Launcher.Answers[3]);
+        }
+        [Test]
+        public void BruteForceAlgorithm_ADACR_ExpectedMinus1()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test4.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BruteForceAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(-1, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BruteForceAlgorithm_AAAAB_Expected5()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test5.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BruteForceAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(5, Launcher.Answers[0]);
+        }
+        ///////////////////////////////////////////
+        [Test]
+        public void BoyerMooreAlgorithm_DDABA_Expected14()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test1.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BoyerMooreAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(14, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BoyerMooreAlgorithm_5_Expected5()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test2.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BoyerMooreAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(4, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BoyerMooreAlgorithm_CAB_Expected2_5_8_11()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test3.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BoyerMooreAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(2, Launcher.Answers[0]);
+            Assert.AreEqual(5, Launcher.Answers[1]);
+            Assert.AreEqual(8, Launcher.Answers[2]);
+            Assert.AreEqual(11, Launcher.Answers[3]);
+        }
+        [Test]
+        public void BoyerMooreAlgorithm_ADACR_ExpectedMinus1()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test4.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BoyerMooreAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(-1, Launcher.Answers[0]);
+        }
+        [Test]
+        public void BoyerMooreAlgorithm_AAAAB_Expected5()
+        {
+            string pathTest = @"D:\projects\Algo\SearchAlgorithm\string_test5.txt";
+            Launcher.ReadWithFile(pathTest);
+
+            Launcher.Answers = (int[])Launcher.BoyerMooreAlgorithm().ToArray().Clone();
+
+            Assert.AreEqual(5, Launcher.Answers[0]);
+        }
+    }
+}
